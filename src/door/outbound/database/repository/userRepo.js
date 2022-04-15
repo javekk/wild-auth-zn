45210f1e8b5e0d05ab77d1e7f14d80f08c56faf3
javekk <periniraffaele@gmail.com>
2022-04-15 09:41:17 +0200

    Add Tap test framework

---
+++ src/door/outbound/database/repository/userRepo.js
@@ -0,0 +1,37 @@
+
+
+// FAKE DB
+const users = [
+    {
+        id: 1,
+        username: 'superadmin',
+        password: 'superadmin',
+        firstName: 'Hans',
+        lastName: 'Von Hammein',
+        role: 'SuperAdmin'
+    },
+    {
+        id: 2,
+        username: 'admin',
+        password: 'admin',
+        firstName: 'Gennaro',
+        lastName: 'Donnola',
+        role: 'Admin'
+    },
+    {
+        id: 3,
+        username: 'user',
+        password: 'user',
+        firstName: 'Franco',
+        lastName: 'Sbaus',
+        role: 'User',
+    },
+    {
+        id: 4,
+        username: 'user',
+        password: 'user',
+        firstName: 'Sergej',
+        lastName: 'Nikolaj',
+        role: 'User',
+    }
+];
